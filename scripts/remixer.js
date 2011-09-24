049dd38174897f50b51b8b42a01f56f7ae421f07
brianchirls <brian@chirls.com>
2011-09-24 16:36:19 -0400

    General code cleanup (jslint). Mostly white space and semicolons, a couple undefined variables

--- scripts/remixer.js
+++ scripts/remixer.js
@@ -1,11 +1,13 @@
 jQuery(function($){
-  
+
+	"use strict";
+
   /*
    * Handy drag n' drop -> Data URL tool
    * by @boazsender: http://boazsender.com
    *
-   */ 
-  
+   */
+
   // jQuery creates it's own event object, and it doesn't have a
   // dataTransfer property yet. This adds dataTransfer to the event object.
   // Thanks to @tbranyen for figuring this out!
@@ -14,15 +16,15 @@ jQuery(function($){
   $('body')
   .bind( 'dragenter dragover', false)
   .bind( 'drop', function( e ) {
+    var droplocation = $(e.target);
     e.stopPropagation();
     e.preventDefault();
-    droplocation = $(e.target);
-    
+
     // Only let videos get dropped from the file system onto one of the bins
     if( droplocation.attr('id') === 'bin1' || droplocation.attr('id') === 'bin2'){
       // Only try to put file(s) in the bins if some have been dragged to the browser
       if( e.dataTransfer && e.dataTransfer.files ){
-  
+
         $.each( e.dataTransfer.files, function(index, file){
           var fileReader = new FileReader();
               fileReader.onload = (function(file) {
@@ -35,12 +37,12 @@ jQuery(function($){
                   .draggable({
                     revert: true
                   })
-                  .appendTo( droplocation )
-                }; 
-              })(file);
+                  .appendTo( droplocation );
+                };
+              }(file));
           fileReader.readAsDataURL(file);
         });
-  
+
       }
     }
   });
@@ -49,17 +51,18 @@ jQuery(function($){
   $('.thumb')
   .draggable({
     revert: true
-  })
+  });
 
   // Make the audio/video sources droppable
   $('#audioSource, #videoSource').droppable({
     drop: function( event, ui ) {
-      var other = (this.id === 'audioSource' ? 'videoSource' : 'audioSource');
+      var otherSource, newSource,
+        other = (this.id === 'audioSource' ? 'videoSource' : 'audioSource');
       other = $('#' + other + ' video');
       if (other.length) {
-        var otherSource = other.attr('class').split(' ')[0];
+        otherSource = other.attr('class').split(' ')[0];
       }
-      var newSource = ui.helper.attr('class').split(' ')[0];
+      newSource = ui.helper.attr('class').split(' ')[0];
 
       if (newSource === otherSource) {
         return;
@@ -76,7 +79,7 @@ jQuery(function($){
       $( this ).html( ui.helper );
     }
   });
-  
+
   // Mashup the content from audio and video sources
   $('#makeMashup').click(function(){
     var audio = $('#audioSource video')[0],
@@ -84,7 +87,7 @@ jQuery(function($){
         video = $('#videoSource video')[0],
         videoSrc = $('#videoSource video').attr('src'),
         actualVideo, actualAudio;
-    
+
     function checkLoaded() {
     	if (!actualVideo || !actualVideo.parentNode || !actualAudio || !actualAudio.parentNode) {
     		return;
@@ -100,9 +103,9 @@ jQuery(function($){
     		setTimeout(checkLoaded, 10);
     	}
     }
-    
+
     $('#mashup').html('');
-    
+
     actualVideo = $('<video>', {
       src: videoSrc,
       id: 'newVideo'
@@ -115,29 +118,26 @@ jQuery(function($){
     })
     .appendTo('#mashup')
     .css({
-     display: 'none' 
+     display: 'none'
     })[0];
 
     $('#canvas').addClass('loading');
-    
+
     checkLoaded();
-    
+
   });
-  
+
   // Swap the audio and video sources
   $('#swapVideos').click(function(){
     var video = $('#videoSource video'),
         audio = $('#audioSource video');
-    
 
-        video.appendTo('#audioSource')
-        audio.appendTo('#videoSource')
-        
-        $('#newAudio, #mashup video').remove()
-        
-    
+        video.appendTo('#audioSource');
+        audio.appendTo('#videoSource');
+
+        $('#newAudio, #mashup video').remove();
   });
-  
+
   // Send everything back where it came from
   $('#clearVideos').click(function( event ){
     var video = $('#videoSource video'),
@@ -150,9 +150,9 @@ jQuery(function($){
 
     if (video.length) {
       video.insertAfter($('#' + video.attr('class').split(' ')[0]).children().eq(0));
-    }    
+    }
   });
-  
+
 /*
   // This doesn't work on <video elements>
   // Lame.
@@ -161,7 +161,6 @@ jQuery(function($){
     event.stopPropagation();
     event.preventDefault();
     $('#audioSource').append( event.dataTransfer.getData('text/html') );
-      
   });
 */
 
